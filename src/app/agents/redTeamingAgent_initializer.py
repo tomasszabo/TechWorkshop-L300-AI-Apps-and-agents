5f1f80bd09169887bc62b86812854d638154e065
Tomas Szabo <tomas.szabo@outlook.com>
2025-12-11 13:08:18 +0100

    added red teaming

---
+++ src/app/agents/redTeamingAgent_initializer.py
@@ -0,0 +1,79 @@
+# Azure imports
+from azure.identity import DefaultAzureCredential
+from azure.ai.evaluation.red_team import RedTeam, RiskCategory, AttackStrategy
+from pyrit.prompt_target import OpenAIChatTarget
+import os
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+# Azure AI Project Information
+azure_ai_project = os.getenv("AZURE_AI_AGENT_ENDPOINT")
+
+# Instantiate your AI Red Teaming Agent
+red_team_agent = RedTeam(
+    azure_ai_project=azure_ai_project,
+    credential=DefaultAzureCredential(),
+    risk_categories=[
+        RiskCategory.Violence,
+        RiskCategory.HateUnfairness,
+        RiskCategory.Sexual,
+        RiskCategory.SelfHarm
+    ],
+    num_objectives=5,
+)
+
+# red_team_agent = RedTeam(
+#     azure_ai_project=azure_ai_project,
+#     credential=DefaultAzureCredential(),
+#     custom_attack_seed_prompts="data/custom_attack_prompts.json",
+# )
+
+# Start Region - Test with a simple chat target function
+
+# def test_chat_target(query: str) -> str:
+#     return "I am a simple AI assistant that follows ethical guidelines. I'm sorry, Dave. I'm afraid I can't do that."
+
+# async def main():
+#     red_team_result = await red_team_agent.scan(target=test_chat_target)
+
+# End Region - Test with a simple chat target function
+
+# Start Region - Test with Azure OpenAI model
+
+# # Configuration for Azure OpenAI model
+# azure_openai_config = {
+#     "azure_endpoint": f"{os.environ.get('AZURE_OPENAI_ENDPOINT')}/openai/deployments/{os.environ.get('gpt_deployment')}/chat/completions",
+#     "api_key": os.environ.get("AZURE_OPENAI_KEY"),
+#     "azure_deployment": os.environ.get("gpt_deployment")
+# }
+
+# async def main():
+# 	red_team_result = await red_team_agent.scan(target=azure_openai_config)
+
+# End Region - Test with Azure OpenAI model
+
+# Start Region - Test with OpenAIChatTarget
+
+chat_target = OpenAIChatTarget(
+    model_name=os.environ.get("gpt_deployment"),
+    endpoint=f"{os.environ.get("gpt_endpoint")}/openai/deployments/{os.environ.get('gpt_deployment')}/chat/completions",
+    api_key=os.environ.get("gpt_api_key"),
+    api_version=os.environ.get("gpt_api_version"),
+)
+
+async def main():
+	red_team_result = await red_team_agent.scan(
+			target=chat_target,
+			scan_name="Red Team Scan - Easy-Moderate Strategies",
+			attack_strategies=[
+					AttackStrategy.Flip,
+					AttackStrategy.ROT13,
+					AttackStrategy.Base64,
+					AttackStrategy.AnsiAttack,
+					AttackStrategy.Tense
+			])
+
+# End Region - Test with OpenAIChatTarget
+
+asyncio.run(main())
